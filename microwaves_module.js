0bc39273e0d5b5bddd7931f43725add5a8149b82
markb500 <81427572+markb500@users.noreply.github.com>
2023-07-17 12:43:37 +0100

    Add files via upload

--- microwaves_module.js
+++ microwaves_module.js
@@ -10,13 +10,13 @@ function microwaves(ctx, ctx2) {
     prevsum = sum;
     switch(sum) {
         case 1:
-            notesLink = "";
+            notesLink = "images/20200421-RadarBk2MicroAE_v1_2-APO.pdf#page=6";
             sumq += "Show the position of the “E” fields, “H” fields and “wall currents” in the cross-section of a cylindrical resonant cavity shown.";
             ctx.drawImage(cavitysect, 0, 0, 500, 300);
             ctx2.drawImage(cavityfields, -13, 0, 500, 300);
             break;
         case 2:
-            notesLink = "";
+            notesLink = "images/20200421-RadarBk2MicroAE_v1_2-APO.pdf#page=6";
             var cavdia = rndgen(30, 60, 0, 5, -1);
             sumq += "For a cylindrical resonant cavity, calculate its resonant&nbsp;frequency&nbsp;(fo) (to 2 decimal places) given that its internal&nbsp;diameter&nbsp;=&nbsp;"
                         + cavdia + "&nbsp;mm and c&nbsp;=&nbsp;300&nbsp;x&nbsp;10<sup>6</sup>&nbsp;m/s";
@@ -32,7 +32,7 @@ function microwaves(ctx, ctx2) {
             suma += "&=\\underline{\\mathbf{" + dp(300 / (cavdia * 2), 2, 2) + "\\ GHz\\ (2\\ dp)}}\\end{aligned}$$";
             break;
         case 3:
-            notesLink = "";
+            notesLink = "images/20200421-RadarBk2MicroAE_v1_2-APO.pdf#page=6";
             var resfreq = rndgen(2, 6, 1, 0.5, -1);
             sumq += "Calculate, to an accuracy of 1 decimal place, the internal diameter of a cylindrical cavity which has a resonant frequency of " 
                         + resfreq + " Ghz, given that c&nbsp;=&nbsp;300&nbsp;x&nbsp;10<sup>6</sup>&nbsp;m/s";
@@ -48,34 +48,34 @@ function microwaves(ctx, ctx2) {
             suma += "\\end{aligned}$$";
             break;
         case 4:
-            notesLink = "";
+            notesLink = "images/20200421-RadarBk2MicroAE_v1_2-APO.pdf#page=7";
             sumq += "For a cylindrical resonant cavity, where are coupling probes and loops positioned and with which energy field do they interact?";
             suma += "Probes are inserted in the top of the cavity interacting with the E field (parallel)<br><br>" + 
                     "Loops are inserted in the side of the cavity interacting with the H field (at 90 degrees)";
             break;
         case 5:
-            notesLink = "";
+            notesLink = "images/20200421-RadarBk2MicroAE_v1_2-APO.pdf#page=7";
             sumq += "For a cylindrical resonant cavity, where are coupling slots positioned and how does their orientation affect their performance?";
             suma += "Slots are cut in the side walls so that they interrupt the wall currents.<br>" + 
                     "At 90 degrees to the wall currents, max radiation;<br>parallel to the wall currents, no radiation.";
             break;
         case 6:
-            notesLink = "";
+            notesLink = "images/20200421-RadarBk2MicroAE_v1_2-APO.pdf#page=9";
             sumq += "What is the function of a Cavity Magnetron?";
             suma += "It is used in the Tx as a high power microwave oscillator.";
             break;
         case 7:
-            notesLink = "";
+            notesLink = "images/20200421-RadarBk2MicroAE_v1_2-APO.pdf#page=10";
             sumq += "What is the function of a Travelling Wave Tube?";
             suma += "It is used as a high gain microwave amplifier in either the transmitter or the receiver.";
             break;
         case 8:
-            notesLink = "";
+            notesLink = "images/20200421-RadarBk2MicroAE_v1_2-APO.pdf#page=11";
             sumq += "What is the function of the Gunn Diode in a varactor tuned microwave resonant cavity oscillator?";
             suma += "It is used in receivers as a low noise, electronically tuneable, microwave local oscillator.";
             break;
         case 9:
-            notesLink = "";
+            notesLink = "images/20200421-RadarBk2MicroAE_v1_2-APO.pdf#page=18";
             sumq += "The diagram shows a Choke Flange Joint. Briefly explain how the gap at C appears to be closed to RF microwave energy travelling along the waveguide.";
             suma += "<br><br><br><br><br><br><br><br><br><br>";
             suma += "Distances AB and BC are both 0.25λ. The short circuit at A is therefore reflected back over 0.5λ to form a short circuit at C. " + 
@@ -83,27 +83,27 @@ function microwaves(ctx, ctx2) {
             ctx.drawImage(chokeflange, 0, 0, 500, 300);
             break;
         case 10:
-            notesLink = "";
+            notesLink = "images/20200421-RadarBk2MicroAE_v1_2-APO.pdf#page=16";
             sumq += "What is the function of a dummy load?";
             suma += "It is used to absorb microwave energy without reflections. Used during servicing.";
             break;
         case 11:
-            notesLink = "";
+            notesLink = "images/20200421-RadarBk2MicroAE_v1_2-APO.pdf#page=17";
             sumq += "Briefly explain the function of a Slot Coupled Attenuator.";
             suma += "It is used to sample the transmitted energy in order to check frequency, power and PRF.";
             break;
         case 12:
-            notesLink = "";
+            notesLink = "images/20200421-RadarBk2MicroAE_v1_2-APO.pdf#page=21";
             sumq += "Describe the beam shape for a Search Radar.";
             suma += "Fan shaped. Narrow in azimuth, broad in elevation.";
             break;
         case 13:
-            notesLink = "";
+            notesLink = "images/20200421-RadarBk2MicroAE_v1_2-APO.pdf#page=21";
             sumq += "For a Pencil beam:<br>a. What type of Radar would use this beam shape?<br>b. What are its characteristics in azimuth & elevation?";
             suma += "a. Airborne Intercept (AI), Terrain Following Radar (TFR) or Tracking Radar.<br>b. Narrow in azimuth, narrow in elevation.";
             break;
         case 14:
-            notesLink = "";
+            notesLink = "images/20200421-RadarBk2MicroAE_v1_2-APO.pdf#page=21";
             sumq += "For a Cosecant-Squared beam:<br>a. What are the beam widths in azimuth and elevation?<br>" + 
                     "b. What type of radar uses this beam shape?<br>c. How does this beam differ from a search radar beam?";
             suma += "a. Narrow in azimuth, broad in elevation.<br>" + 
@@ -112,17 +112,17 @@ function microwaves(ctx, ctx2) {
                             "target size in the resulting display.";
             break;
         case 15:
-            notesLink = "";
+            notesLink = "images/20200421-RadarBk2MicroAE_v1_2-APO.pdf#page=23";
             sumq += "What is a parabolic reflector?";
             suma += "It is a curved, bowl-shaped, aerial which produces a pencil beam";
             break;
         case 16:
-            notesLink = "";
+            notesLink = "images/20200421-RadarBk2MicroAE_v1_2-APO.pdf#page=23";
             sumq += "What is a slotted array?";
             suma += "A flat aerial which can radiate from a large number of elements that are in the same plane.";
             break;
         case 17:
-            notesLink = "";
+            notesLink = "images/20200421-RadarBk2MicroAE_v1_2-APO.pdf#page=25";
             sumq += "What is a active array?";
             suma += "It is an array of miniature transmitter/receiver units fixed directly to the front of the dish, " + 
                     "which are individually controlled by the radar systems computer.";
